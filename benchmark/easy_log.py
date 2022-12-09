e1322b6a68ba656c7d7d74a4dc544cf7800aed03
Manuel Vidigueira <mvidigueira@gmail.com>
2022-12-09 16:50:54 +0000

    Fix sample benchmarking to measure for higher payload sizes

--- benchmark/easy_log.py
+++ benchmark/easy_log.py
@@ -1,7 +1,12 @@
 from benchmark.logs import ParseError, LogParser
+import sys
 
 ''' Print a summary of the logs '''
 try:
-    print(LogParser.chop_process('./logs', faults='?').result())
+    n = len(sys.argv)
+    if n < 2:
+        print("Must provide the logs directory path")
+    else:
+        print(LogParser.chop_process(sys.argv[1], faults='?').result())
 except ParseError as e:
     pass
